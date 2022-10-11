64faeba038dc726ecbfa3a6bf0f80e6641a48dd4
prescillaanuop <prescillaanuop@gmail.com>
2022-10-11 19:37:13 +0200

    five

---
+++ index.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let imgCtn = document.querySelectorAll("#imgCtn > img");
+
+function callanimation() {
+  imgCtn.style.animation = "display 10s infinite";
+}
