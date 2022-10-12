eaf434428ab8bc9f74ef1ff43f9245f2b3ebbf23
prescillaanuop <prescillaanuop@gmail.com>
2022-10-12 14:23:16 +0200

    eleven

--- index.js
+++ index.js
@@ -11,34 +11,66 @@ let imgCtn7 = document.querySelector("#imgCtn img:nth-child(8)");
 let imgCtn8 = document.querySelector("#imgCtn img:nth-child(9)");
 let imgCtn9 = document.querySelector("#imgCtn img:nth-child(10)");
 
+let img1 = document.getElementById("img1");
+
 function callanimation() {
-  imgCtn1.style.animation = "display 10s infinite";
-  imgCtn2.style.animation = "display 10s infinite";
+  imgCtn1.style.animation = "display 18s infinite";
+  imgCtn2.style.animation = "display 18s infinite";
   imgCtn2.style.animationDelay = "2s";
-  imgCtn3.style.animation = "display 10s infinite";
+  imgCtn3.style.animation = "display 18s infinite";
   imgCtn3.style.animationDelay = "4s";
-  imgCtn4.style.animation = "display 10s infinite";
+  imgCtn4.style.animation = "display 18s infinite";
   imgCtn4.style.animationDelay = "6s";
-  imgCtn5.style.animation = "display 10s infinite";
+  imgCtn5.style.animation = "display 18s infinite";
   imgCtn5.style.animationDelay = "8s";
-  imgCtn6.style.animation = "display 10s infinite";
+  imgCtn6.style.animation = "display 18s infinite";
   imgCtn6.style.animationDelay = "10s";
-  imgCtn7.style.animation = "display 10s infinite";
+  imgCtn7.style.animation = "display 18s infinite";
   imgCtn7.style.animationDelay = "12s";
-  imgCtn8.style.animation = "display 10s infinite";
+  imgCtn8.style.animation = "display 18s infinite";
   imgCtn8.style.animationDelay = "14s";
-  imgCtn9.style.animation = "display 10s infinite";
+  imgCtn9.style.animation = "display 18s infinite";
   imgCtn9.style.animationDelay = "16s";
 }
 
 function callrevanimation() {
-  imgCtn1.style.animation = "displayrev 10s infinite";
-  imgCtn2.style.animation = "displayrev 10s infinite";
+  imgCtn1.style.animation = "displayrev 18s infinite";
+  imgCtn2.style.animation = "displayrev 18s infinite";
   imgCtn2.style.animationDelay = "2s";
-  imgCtn3.style.animation = "displayrev 10s infinite";
+  imgCtn3.style.animation = "displayrev 18s infinite";
   imgCtn3.style.animationDelay = "4s";
-  imgCtn4.style.animation = "displayrev 10s infinite";
+  imgCtn4.style.animation = "displayrev 18s infinite";
   imgCtn4.style.animationDelay = "6s";
-  imgCtn5.style.animation = "displayrev 10s infinite";
+  imgCtn5.style.animation = "displayrev 18s infinite";
   imgCtn5.style.animationDelay = "8s";
+  imgCtn6.style.animation = "displayrev 18s infinite";
+  imgCtn6.style.animationDelay = "10s";
+  imgCtn7.style.animation = "displayrev 18s infinite";
+  imgCtn7.style.animationDelay = "12s";
+  imgCtn8.style.animation = "displayrev 18s infinite";
+  imgCtn8.style.animationDelay = "14s";
+  imgCtn9.style.animation = "displayrev 18s infinite";
+  imgCtn9.style.animationDelay = "16s";
+}
+
+function showEarth() {
+  window.location.href = "planets.html#earth";
+}
+function showSun() {
+  window.location.href = "planets.html#sun";
+}
+function showMars() {
+  window.location.href = "planets.html#mars";
+}
+function showJupiter() {
+  window.location.href = "planets.html#jupiter";
+}
+function showSaturn() {
+  window.location.href = "planets.html#saturn";
+}
+function showVenus() {
+  window.location.href = "planets.html#venus";
+}
+function showMercury() {
+  window.location.href = "planets.html#mercury";
 }
