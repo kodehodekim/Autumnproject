7aa756ee690fee0be0f571a5acd7ccfd550d69e7
kodehodekim <kim.kodehode@gmail.com>
2022-10-13 13:57:52 +0200

    lol

--- Planets/planets.js
+++ Planets/planets.js
@@ -1,48 +1,25 @@
-// function imgup() {
-//     document.getElementsByName('').style.height = "900px";
+/* Expand and collapse the images */
+let planetIsCollapsed = true;
 
-//  } else if document.getElementById('image').style.height = "900px"; {
-//     document.getElementById('image').style.height = "500px";
-//  }
+const pAnims = document.getElementsByClassName("pAnim");
 
-// if (max-height = "400px";) {
-//     planet2()
-// }
+for (const pAnimEl of pAnims) {
+  pAnimEl.addEventListener("click", function () {
+    const pTextEl = pAnimEl.querySelector(".pText");
 
-// if (max-height = "900px";) {
-//     document.getElementById("planet2txt").innerHTML = "PLease click me"
-//     let max-height = "400px"
-
-let planet2IsCollapsed = true;
-const planetEl = document.getElementById("planet2");
-const planetElText = document.getElementById("planet2text");
-
-function planet2() {
-  if (planet2IsCollapsed) {
-    planetElText.style.fontSize = "12px";
-    planetElText.textContent = "Click me to shrink";
-    planetEl.style.transitionDuration = "1.5s";
-    planetEl.style.minHeight = "80%";
-    planet2IsCollapsed = false;
-  } else {
-    planetElText.textContent = "Mercury";
-    planetElText.style.fontSize = "50px";
-    planetEl.style.transitionDuration = "1.5s";
-    planetEl.style.minHeight = "40%";
-    planet2IsCollapsed = true;
-  }
+    if (!pAnimEl.classList.contains("collapsed")) {
+      pTextEl.textContent = "Click to shrink";
+      pTextEl.style.fontSize = "12px";
+      pAnimEl.style.transitionDuration = "1.5s";
+      pAnimEl.style.minHeight = "80%";
+      pAnimEl.classList.add("collapsed");
+      console.log("Expanded");
+    } else {
+      pTextEl.textContent = "";
+      pAnimEl.style.minHeight = "40%";
+      pAnimEl.style.transitionDuration = "1.5s";
+      pAnimEl.classList.remove("collapsed");
+      console.log("collapsed");
+    }
+  });
 }
-
-function planet3() {}
-
-function planet4() {}
-
-function planet5() {}
-
-function planet6() {}
-
-function planet7() {}
-
-function planet8() {}
-
-function planet9() {}
